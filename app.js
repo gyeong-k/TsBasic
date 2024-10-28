6ccdda0dd772b4d67d7d1e5a63914094e10ca500
gayeongkim <gy.kim.g0@gmail.com>
2024-10-28 18:07:34 +0900

    객체를 반환하는 코드

---
+++ app.js
@@ -0,0 +1,9 @@
+var stdId = 1111;
+var stdName = "lee";
+var age = 20;
+var gender = "male";
+var course = "typescript";
+var completed = false;
+function getInfo(id) {
+    return null;
+}
---
+++ app.ts
@@ -0,0 +1,17 @@
+let stdId: number = 1111;
+let stdName: string = "lee";
+let age: number = 20;
+let gender: string = "male";
+let course: string = "typescript";
+let completed: boolean = false;
+
+function getInfo(id: number): {
+  stdId: number;
+  stdName: string;
+  age: number;
+  gender: string;
+  course: string;
+  completed: boolean;
+} {
+  return null;
+}
