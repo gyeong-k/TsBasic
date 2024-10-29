6dd7c84482646eb3c50876b44c76ceca7b7cccb3
gayeongkim <gy.kim.g0@gmail.com>
2024-10-29 18:08:55 +0900

    생성자

--- oop.js
+++ oop.js
@@ -2,17 +2,22 @@
 //멤버함수 = 메소드
 //클래스
 var Employee = /** @class */ (function () {
-    function Employee() {
+    //생성자 -> 객체 생성시 자동 생성
+    //다른 언어에서 생성자는 클래스 이름과 같음
+    //this = 객체 자기자신
+    function Employee(empName, age, empjob) {
         var _this = this;
         this.printEmp = function () {
             console.log("".concat(_this.empName, "\uC758 \uB098\uC774\uB294 ").concat(_this.age, "\uC774\uACE0, \uC9C1\uC5C5\uC740 ").concat(_this.empjob, "\uC785\uB2C8\uB2E4."));
         };
+        this.empName = empName;
+        this.age = age;
+        this.empjob = empjob;
     }
     return Employee;
 }());
 //객체
-var employee1 = new Employee();
-employee1.empName = "kim";
-employee1.age = 20;
-employee1.empjob = "개발자";
+//다른 언어의 생성자는 클래스 이름과 같다는 걸 기억
+//그럼 아래 코드의 Employee는 생성자라고 생각해도 무관
+var employee1 = new Employee("kim", 20, "개발자");
 employee1.printEmp();
--- oop.ts
+++ oop.ts
@@ -7,6 +7,16 @@ class Employee {
   empName: string;
   age: number;
   empjob: string;
+
+  //생성자 -> 객체 생성시 자동 생성
+  //다른 언어에서 생성자는 클래스 이름과 같음
+  //this = 객체 자기자신
+  constructor(empName: string, age: number, empjob: string) {
+    this.empName = empName;
+    this.age = age;
+    this.empjob = empjob;
+  }
+
   printEmp = (): void => {
     console.log(
       `${this.empName}의 나이는 ${this.age}이고, 직업은 ${this.empjob}입니다.`
@@ -15,8 +25,8 @@ class Employee {
 }
 
 //객체
-let employee1 = new Employee();
-employee1.empName = "kim";
-employee1.age = 20;
-employee1.empjob = "개발자";
+//다른 언어의 생성자는 클래스 이름과 같다는 걸 기억
+//그럼 아래 코드의 Employee는 생성자라고 생각해도 무관
+let employee1 = new Employee("kim", 20, "개발자");
+
 employee1.printEmp();
